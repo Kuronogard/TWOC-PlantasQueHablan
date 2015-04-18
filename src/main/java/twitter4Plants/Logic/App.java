363082e08cb3e1e947a8726ee1d3734e2a3e0d4e
Alvarocaste <alvaro.castellanos.lopez@gmail.com>
2015-04-18 18:22:14 +0200

    Fixed imports in App.java

--- src/main/java/twitter4Plants/Logic/App.java
+++ src/main/java/twitter4Plants/Logic/App.java
@@ -1,22 +1,13 @@
 package twitter4Plants.Logic;
 
-import java.util.List;
-
 import twitter4Plants.TwitterSender.Sender;
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.User;
-import twitter4j.conf.ConfigurationBuilder;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
         Sender sender = Sender.getInstance();
         sender.connect();
         sender.toTweet("Seguimos probando para que las plantas hablen #twoc15");
