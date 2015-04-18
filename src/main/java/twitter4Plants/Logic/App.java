f371177d57dc5c22c4479fbb5fa64e9e49841e9a
Alvarocaste <alvaro.castellanos.lopez@gmail.com>
2015-04-18 18:15:24 +0200

    Added Sender.java logic and refactored App.java

--- src/main/java/twitter4Plants/Logic/App.java
+++ src/main/java/twitter4Plants/Logic/App.java
@@ -2,6 +2,7 @@ package twitter4Plants.Logic;
 
 import java.util.List;
 
+import twitter4Plants.TwitterSender.Sender;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -15,30 +16,9 @@ import twitter4j.conf.ConfigurationBuilder;
  */
 public class App 
 {
-    public static void main( String[] args ) throws TwitterException {
-        try {
-            ConfigurationBuilder cb = new ConfigurationBuilder();
-            cb.setDebugEnabled(true)
-              .setOAuthConsumerKey("y1zM2z2cnqfo80qLuMcdAGP4b")
-              .setOAuthConsumerSecret("7GYuaitHZ5d0MmoSk2nTHf9NICOJv6hGxBRF1Bqb3v8CLpxIS9")
-              .setOAuthAccessToken("3179195939-VwKPvlLgnkMFOpYqp8Mph9e9q4geuCZC3t6GcjA")
-              .setOAuthAccessTokenSecret("pNDDi7kdb9BB14RrJGRz9ide9v3uVEqECtlEhJ6T5mt4g");
-            TwitterFactory tf = new TwitterFactory(cb.build());
-            Twitter twitter = tf.getInstance();
-            // gets Twitter instance with default credentials
-            User user = twitter.verifyCredentials();
-            String message = "Soy una planta que habla y esto parece que marcha #twoc15";
-            Status status = twitter.updateStatus(message);
-            
-//            List<Status> statuses = twitter.getHomeTimeline();
-//            System.out.println("Showing @" + user.getScreenName() + "'s home timeline.");
-//            for (Status status : statuses) {
-//                System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-//            }
-        } catch (TwitterException te) {
-            te.printStackTrace();
-            System.out.println("Failed to get timeline: " + te.getMessage());
-            System.exit(-1);
-        }
+    public static void main( String[] args ) {
+        Sender sender = Sender.getInstance();
+        sender.connect();
+        sender.toTweet("Seguimos probando para que las plantas hablen #twoc15");
     }
 }
--- src/main/java/twitter4Plants/TwitterSender/Sender.java
+++ src/main/java/twitter4Plants/TwitterSender/Sender.java
@@ -1,5 +1,67 @@
 package twitter4Plants.TwitterSender;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class Sender {
+    private static Sender instance = null;
+    private final int MAX_NUMBER_OF_ATTEMPTS = 3;
+    private Twitter twitter;
+    private int attemps = 0;
+
+    protected Sender() {
+    }
+
+    public static Sender getInstance() {
+        if (isInstanceNull()) {
+            instance = new Sender();
+        }
+        return instance;
+    }
+
+    public void connect() {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("y1zM2z2cnqfo80qLuMcdAGP4b")
+                .setOAuthConsumerSecret(
+                        "7GYuaitHZ5d0MmoSk2nTHf9NICOJv6hGxBRF1Bqb3v8CLpxIS9")
+                .setOAuthAccessToken(
+                        "3179195939-VwKPvlLgnkMFOpYqp8Mph9e9q4geuCZC3t6GcjA")
+                .setOAuthAccessTokenSecret(
+                        "pNDDi7kdb9BB14RrJGRz9ide9v3uVEqECtlEhJ6T5mt4g");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        twitter = tf.getInstance();
+    }
+
+    public void toTweet(String message) {
+        try {
+            if (this.canSend()) {
+                twitter.updateStatus(message);
+                this.resetAttemps();
+            }
+        } catch (TwitterException e) {
+            this.doAttemp();
+            this.toTweet(message);
+        }
+    }
+
+    private static boolean isInstanceNull() {
+        return instance == null;
+    }
+
+    private void doAttemp() {
+        attemps += 1;
+    }
+
+    private void resetAttemps() {
+        attemps = 0;
+
+    }
+
+    private boolean canSend() {
+        return this.attemps != MAX_NUMBER_OF_ATTEMPTS;
+    }
 
 }
