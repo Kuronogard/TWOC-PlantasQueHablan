44f1ac787b0f7812c62c008bb8e26d335356ee3e
elhombreseta <guveal@gmail.com>
2015-04-18 19:59:15 +0100

    Implementation of the DAO's

--- src/main/java/twitter4Plants/DAO/DummyServerDAO.java
+++ src/main/java/twitter4Plants/DAO/DummyServerDAO.java
@@ -3,6 +3,7 @@ package twitter4Plants.DAO;
 import twitter4Plants.Plants.PlantMeta;
 import twitter4Plants.Plants.PlantType;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class DummyServerDAO.
  */
@@ -15,7 +16,7 @@ public class DummyServerDAO implements ServerDAO {
 	 */
 	@Override
 	public PlantType getPlantType(int idPlantType) {
-		
+
 		double temperatureMax = 0;
 		double temperatureMin = 0;
 		double temperatureHappy = 0;
@@ -38,14 +39,39 @@ public class DummyServerDAO implements ServerDAO {
 	 */
 	@Override
 	public PlantMeta getPlantMeta(int idPlantMeta) {
-		
+
 		int plantId = 0;
 		int ownerId = 0;
 		String plantName = "";
 		String plantDescription = "";
 		String ownerTwitter = "";
-		PlantMeta plantMeta = new PlantMeta(plantId, ownerId, plantName, plantDescription, ownerTwitter);
+		PlantMeta plantMeta = new PlantMeta(plantId, ownerId, plantName,
+				plantDescription, ownerTwitter);
 		return plantMeta;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * twitter4Plants.DAO.ServerDAO#savePlantType(twitter4Plants.Plants.PlantType
+	 * )
+	 */
+	@Override
+	public void savePlantType(PlantType plantType) {
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * twitter4Plants.DAO.ServerDAO#savePlantMeta(twitter4Plants.Plants.PlantMeta
+	 * )
+	 */
+	@Override
+	public void savePlantMeta(PlantMeta plantMeta) {
+
 	}
 
 }
--- src/main/java/twitter4Plants/DAO/ServerDAO.java
+++ src/main/java/twitter4Plants/DAO/ServerDAO.java
@@ -3,6 +3,7 @@ package twitter4Plants.DAO;
 import twitter4Plants.Plants.PlantMeta;
 import twitter4Plants.Plants.PlantType;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Interface ServerDAO.
  */
@@ -18,6 +19,12 @@ public interface ServerDAO {
 	public PlantType getPlantType(int idPlantType);
 
 	/**
+	 * Save plant type.
+	 *
+	 * @param plantType the plant type
+	 */
+	public void savePlantType(PlantType plantType );
+	/**
 	 * Gets the plant meta.
 	 *
 	 * @param idPlantMeta
@@ -26,4 +33,11 @@ public interface ServerDAO {
 	 */
 	public PlantMeta getPlantMeta(int idPlantMeta);
 
+	/**
+	 * Save plant meta.
+	 *
+	 * @param plantMeta the plant meta
+	 */
+	public void savePlantMeta(PlantMeta plantMeta);
+	
 }
