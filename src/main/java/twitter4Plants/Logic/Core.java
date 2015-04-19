9660b3541829e551b085183fddbb42c2e9507f60
Kuronogard <danielpintoriv@gmail.com>
2015-04-19 12:45:42 +0200

    Added a little terminal to test database

--- src/main/java/twitter4Plants/Logic/Core.java
+++ src/main/java/twitter4Plants/Logic/Core.java
@@ -11,8 +11,12 @@ import twitter4Plants.Plants.PlantType;
 import twitter4Plants.TwitterSender.Sender;
 
 import javax.swing.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Timestamp;
 
 public class Core {
@@ -63,6 +67,79 @@ public class Core {
         });
 
         timer.start();
-        while(true) {}
+        
+        command_terminal();
+        
+    }
+
+    private void command_terminal(){
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader (isr);
+        String command = new String();
+        PlantMeta meta;
+        PlantType type;
+        String plantName, ownerTwitter;
+        int plantID, typeID;
+        double humidityMax, humidityMin, humidityHappy;
+        double temperatureMax, temperatureMin, temperatureHappy;
+        double lightMax, lightMin, lightHappy;
+        
+    	
+        while(true){
+	        try {
+				command = br.readLine();
+				
+		        switch(command){
+	        	case "addPlant":
+	        		System.out.print("plant ID: ");
+	        		plantID = Integer.parseInt(br.readLine());
+	        		System.out.print("type ID: ");
+	        		typeID = Integer.parseInt(br.readLine());
+	        		System.out.print("plant name: ");
+	        		plantName = br.readLine();
+	        		System.out.print("owner twitter: ");
+	        		ownerTwitter = br.readLine();
+	        		meta = new PlantMeta(plantID, typeID, plantName, ownerTwitter);
+	        		serverdao.savePlantMeta(meta);
+	        		break;
+	        	case "addPlantType":
+	        		System.out.print("plant ID: ");
+	        		plantID = Integer.parseInt(br.readLine());
+	        		System.out.print("Humidity Max: ");
+	        		humidityMax = Double.parseDouble(br.readLine());
+	        		System.out.print("Humidity Min: ");
+	        		humidityMin = Double.parseDouble(br.readLine());
+	        		System.out.print("Humidity Happy: ");
+	        		humidityHappy = Double.parseDouble(br.readLine());
+	        		System.out.print("Temperature Max: ");
+	        		temperatureMax = Double.parseDouble(br.readLine());
+	        		System.out.print("Temperature Min: ");
+	        		temperatureMin = Double.parseDouble(br.readLine());
+	        		System.out.print("Temperature Happy: ");
+	        		temperatureHappy = Double.parseDouble(br.readLine());
+	        		System.out.print("Light Max: ");
+	        		lightMax = Double.parseDouble(br.readLine());
+	        		System.out.print("Light Min: ");
+	        		lightMin = Double.parseDouble(br.readLine());
+	        		System.out.print("Light Happy: ");
+	        		lightHappy = Double.parseDouble(br.readLine());  
+	        		type = new PlantType(temperatureMax, temperatureMin, temperatureHappy,
+	        				humidityMax, humidityMin, humidityHappy,
+	        				lightMax, lightMin, lightHappy);
+	        		serverdao.savePlantType(type);
+	        		break;
+	        	default:
+	        		System.out.println(command.concat(" Not an existent command."));
+	        }
+				
+			} catch (IOException e1) {
+				System.err.println("Console is not working.");
+				e1.printStackTrace();
+			} catch(NumberFormatException e){
+				System.err.println("A param is not correct.");
+				e.printStackTrace();
+			}
+    
+        }
     }
 }
